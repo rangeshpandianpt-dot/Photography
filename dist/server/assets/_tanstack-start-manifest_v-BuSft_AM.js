f75b60d042d8a9c3f1ac8d813894b89a6f07e34b
RANGESH <rangeshpandianpt@gmail.com>
2026-06-07 19:38:02 +0530

    Add files via upload

---
+++ dist/server/assets/_tanstack-start-manifest_v-BuSft_AM.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Web Clients/Photography/satiz-prestige-main/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-rkfezg5s.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-rkfezg5s.js" } }] }, "/": { filePath: "D:/Web Clients/Photography/satiz-prestige-main/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CAm73-c0.js"] } } });
+export {
+  tsrStartManifest
+};
